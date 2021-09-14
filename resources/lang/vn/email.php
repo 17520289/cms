9cafebdc4ad0bfa51c5af73681d8bbb8b82b67bf
17520289 <chautv@hlsolutions.jp>
2021-09-14 18:15:45 +0700

    [Edric][#56][FIX][Translate page reset pass and email]

--- resources/lang/vn/email.php
+++ resources/lang/vn/email.php
@@ -24,11 +24,11 @@ return array(
   ),
     'resetPassword' =>
         array(
-            'subject' => 'Reset Password Notification',
-            'text' => 'You are receiving this email because we received a password reset request for your account.',
-            'text2' => 'If you did not request a password reset, no further action is required.',
-            'button' => 'Reset Password',
-            'expire' => 'This password reset link will expire in :count minutes.',
+            'subject' => 'Thông báo đặt lại mật khẩu',
+            'text' => '  Bạn nhận được email này vì chúng tôi đã nhận được yêu cầu đặt lại mật khẩu cho tài khoản của bạn.',
+            'text2' => 'Nếu bạn không yêu cầu đặt lại mật khẩu, bạn không cần thực hiện thêm hành động nào.',
+            'button' => 'Đặt lại mật khẩu',
+            'expire' => 'Link đặt lại mật khẩu sẽ hết hạn trong :count phút.',
         ),
   'newNotice' =>
   array(
